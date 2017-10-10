e56c6c307b0b1ebe5942591d574b2fafb90d2fbe
vkonireddy <vkonireddy@ESITLN83.surfbi.local>
2017-10-10 11:31:00 +0530

    vijay

---
+++ js/custom/asideMenu.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+var rootPath="vijay9677.github.io/";
+
+
+var menuJson={
+"menuItems":[{
+	"menuName":"Java",
+	"url":"java/index.html"
+},{
+	"menuName":"SQL",
+	"url":"sql/index.html"
+},]	
+		
+}
