18e5377c96fbd879033852d7e935a9b6f97a98f4
vkonireddy <vkonireddy@ESITLN83.surfbi.local>
2017-10-16 23:14:31 +0530

    VIJAY COMMIT

---
+++ java/js/JavaMenu.js
@@ -0,0 +1,20 @@
+/**
+ * VIJAY KUMAR REDDY K
+ */
+
+var menuItems=	[{
+		"name":"INTRODUCTION",
+		"url":"introduction.html"
+},{
+	"name":"Features",
+	"url":"features.html"
+	
+},{
+	"name":"Configuration or setting environment variables",
+	"url":"environmwntConfig.html"
+	
+},{
+	"name":"Representation of an Object",
+	"url":"environmwntConfig.html"
+	
+}]
